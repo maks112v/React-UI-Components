e1a2fbe8577fcf9bb6d22b198434e948b5caf8e5
Maksim V <maks112v@gmail.com>
2019-02-11 18:09:28 -0500

    Added moment js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.css';
 
+import Moment from 'react-moment';
+
 function HeaderTitle(){
   return (
-    <div><b>Lambda School</b> <span className="text-muted"><a href="">@LambdaSchool</a> 26 Jan</span></div>
+    <div><b>Lambda School</b> <span className="text-muted"><a href="">@LambdaSchool</a> <Moment format="MMM Do YY" /></span></div>
   );
 }
 
