caea97471814692e0bb32c8bdeb504c322bb7fa9
Maksim V <maks112v@gmail.com>
2019-02-12 15:11:32 -0500

    added

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.jsx
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.jsx
@@ -4,10 +4,10 @@ import './Footer.css';
 function Footer(){
   return(
     <footer>
-      <a href=""><i class="fas fa-comment"></i> 10</a>
-      <a href=""><i class="fas fa-heart"></i> 48</a>
-      <a href=""><i class="fas fa-retweet"></i> 60</a>
-      <a href=""><i class="fas fa-envelope-open"></i></a>
+      <a href=""><i className="fas fa-comment"></i> 10</a>
+      <a href=""><i className="fas fa-heart"></i> 48</a>
+      <a href=""><i className="fas fa-retweet"></i> 60</a>
+      <a href=""><i className="fas fa-envelope-open"></i></a>
     </footer>
   );
 }
